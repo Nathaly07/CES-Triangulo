d9760bf03bad89fce93990aa37921561c4ae555c
Cristofer Paucar <dan.paucar@hotmail.com>
2024-02-01 00:13:28 -0500

    Inserción de funcionalidad: ingreso de datos por consola y cálculo del área.

--- src/triangulo/Triangulo.java
+++ src/triangulo/Triangulo.java
@@ -4,6 +4,8 @@
  */
 package triangulo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author LabP3E003
@@ -16,17 +18,43 @@ public class Triangulo {
         this.lado2=lado2;
         this.lado3=lado3;
     }
-    
-    public double calcularArea(){
-        double area;
-        return 0;
+
+    public static double calcularAreaTriangulo(double lado1, double lado2, double lado3) {
+        double semiPerimetro = (lado1 + lado2 + lado3) / 2;
+
+        // Fórmula de Herón para el área del triángulo
+        double area = Math.sqrt(semiPerimetro * (semiPerimetro - lado1) * (semiPerimetro - lado2) * (semiPerimetro - lado3));
+
+        return area;
     }
-    
-    /**
-     * @param args the command line arguments
-     */
+
+
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        boolean continuar = true;
+
+        while (continuar) {
+            System.out.println("Ingrese el lado 1 del triángulo: ");
+            double lado1 = scanner.nextDouble();
+
+            System.out.println("Ingrese el lado 2 del triángulo: ");
+            double lado2 = scanner.nextDouble();
+
+            System.out.println("Ingrese el lado 3 del triángulo: ");
+            double lado3 = scanner.nextDouble();
+
+
+            double area = calcularAreaTriangulo(lado1, lado2, lado3);
+            System.out.println("El área del triángulo es: " + area);
+
+
+            System.out.println("¿Desea calcular el área de otro triángulo? (Ingrese 'si' para continuar, 'no' para salir): ");
+            String respuesta = scanner.next().toLowerCase();
+
+            if (respuesta.equals("no")) {
+                continuar = false;
+            }
+        }
     }
     
 }
